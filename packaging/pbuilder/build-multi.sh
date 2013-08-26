64636ea5abd47e116491d25bdea97179fe8620e0
MarkS <marksvc@gmail.com>
2013-08-26 14:33:02 -0600

    Add FieldWorks packaging scripts
    Change-Id: Ieef92e17abe532f19421d417cffedbd6bf9b2e33

---
+++ packaging/pbuilder/build-multi.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+NOOP=
+
+if [ "$1" = "-n" ]
+then
+	NOOP=:
+	shift
+fi
+
+cpuarch()
+{
+	case $1 in
+	amd64) echo x86_64;;
+	*)     echo $1;;
+	esac
+}
+
+get_field() # FIELD SRC
+{
+	if grep -q "BEGIN PGP SIGNED MESSAGE" $2
+	then
+		DECRYPT="gpg -d"
+	else
+		DECRYPT="cat"
+	fi
+
+	$DECRYPT $2 2>/dev/null | grep-dctrl -s$1 -n :
+}
+
+has_arch() # ARCH SRC
+{
+	for A in $(get_field Architecture $2)
+	do
+		if [ $A = $1 -o $A = any ]
+		then
+			return 0
+		fi
+	done
+	return 1
+}
+
+binaries()
+{
+	get_field Binary $1 | sed 's/,//g'
+}
+
+export DIST ARCH
+
+for SRC
+do
+	for DIST in ${DISTRIBUTIONS-precise quantal raring} # jessie}
+	do
+		for ARCH in ${ARCHES-amd64 i386}
+		do
+			PACKAGE=${SRC%.dsc}
+			RESULT="$HOME/pbuilder/$DIST/$ARCH/result"
+			DEB1="$RESULT/${PACKAGE}_${ARCH}.deb"
+			DEB2="$RESULT/${PACKAGE}+${DIST}1_${ARCH}.deb"
+
+			OPTS=()
+
+			# Build arch-independent packages only once
+			if [ $ARCH != amd64 ]
+			then
+				OPTS+="--binary-arch"
+
+				# Don't build if not for this arch
+				if ! has_arch $ARCH $SRC
+				then
+					continue
+				fi
+			fi
+
+			if [ $SRC -nt $DEB1 -a $SRC -nt $DEB2 ]
+			then
+				echo PACKAGE=$PACKAGE DIST=$DIST ARCH=$ARCH
+				$NOOP setarch $(cpuarch $ARCH) pbuilder --build "${OPTS[@]}" $SRC
+				echo $? | $NOOP tee $RESULT/${PACKAGE}_$ARCH.status
+				$NOOP rm -f $RESULT/${PACKAGE}.{dsc,{debian.,orig.,}tar.gz}
+			fi
+		done
+	done
+done
---
+++ packaging/pbuilder/setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# setup.sh
+# Setup or update mirrors
+
+set -e
+
+# Process arguments.
+while (( $# )); do
+	case $1 in
+		# Process individual arguments here. Use shift and $1 to get an argument value.
+		--update) update=true ;;
+		*) echo "Error: Unexpected argument \"$1\". Exiting." ; exit 1 ;;
+	esac
+	shift
+done
+
+cd "$(dirname "$0")"
+
+HERE=$(/bin/pwd)
+
+for D in ${DISTRIBUTIONS-precise quantal raring} # jessie}
+do
+	for A in ${ARCHES-amd64 i386}
+	do
+		[ -e $D/$A/base.tgz -a -z "$update" ] && continue
+		[ ! -e $D/$A/base.tgz -a -n "$update" ] && continue
+
+		echo "Processing $D/$A"
+
+		mkdir -p $D/$A/{aptcache,build,result}
+		[ ! -e $D/$A/result/Packages ] && touch $D/$A/result/Packages
+
+		OTHERMIRROR="deb file:$HERE/$D/$A/result/ ./"
+
+		case $D in precise|quantal|raring)
+			MIRROR="http://ubuntu.mirror.rafal.ca/ubuntu/"
+			COMPONENTS="main universe multiverse"
+			KEYRING1="/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+			PROXY="$http_proxy"
+			for S in proposed backports updates; do
+				OTHERMIRROR+="|deb $MIRROR $D-$S $COMPONENTS"
+			done
+			LLSO="http://linux.lsdev.sil.org/ubuntu/"
+			KEYRING2="$HOME/pbuilder/sil.gpg"
+			for S in "" "-experimental"; do
+				OTHERMIRROR+="|deb $LLSO $D$S $COMPONENTS"
+			done
+			;;
+		*)
+			MIRROR="$local_debian_mirror"
+			COMPONENTS="main"
+			LLSO="http://linux.lsdev.sil.org/debian/"
+			KEYRING1=$KEYRING2
+			KEYRING2=""
+			OTHERMIRROR+="|deb $LLSO $D $COMPONENTS"
+			PROXY=""
+		esac
+
+		if [ -z "$update" ]; then
+			options="--create --debootstrap debootstrap"
+		else
+			options="--update --override-config"
+		fi
+
+		sudo HOME=~ DIST=$D ARCH=$A pbuilder $options \
+			${KEYRING1:+--debootstrapopts --keyring=}$KEYRING1 \
+			${KEYRING2:+--keyring }$KEYRING2 \
+			--extrapackages "apt-utils devscripts lsb-release" \
+			--othermirror "$OTHERMIRROR" \
+			--mirror "$MIRROR" \
+			--components "$COMPONENTS" \
+			${PROXY:+--http-proxy }$PROXY
+	done
+done
