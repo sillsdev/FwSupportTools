da105fef1b014dc85a8954447295aa58653dd455
Eberhard Beilharz <eb1@sil.org>
2017-06-28 10:46:29 +0200

    [packaging] Update list of supported distros
    Change-Id: Ia855cc7ae8c8535e04068ba259f9a3850c920ced

--- packaging/pbuilder/common.sh
+++ packaging/pbuilder/common.sh
@@ -6,9 +6,11 @@ init()
 	[ -e $CONFIGFILE ] && . $CONFIGFILE # Settings for PBUILDERDIR, DISTRIBUTIONS, etc.
 
 	# currently supported and future Ubuntu versions
-	UBUNTU_DISTROS="precise trusty vivid wily xenial"
+	UBUNTU_DISTROS="precise trusty xenial zasty artful"
 	# no longer supported Ubuntu versions that live in old-releases.ubuntu.com
-	UBUNTU_OLDDISTROS="quantal raring saucy utopic"
+	UBUNTU_OLDDISTROS=""
+	# We're no longer building packages for: quantal raring saucy utopic vivid wily yakkety
+
 	# Debian versions
 	DEBIAN_DISTROS="wheezy jessie stretch"
 
