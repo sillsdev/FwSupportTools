90db9335aed5f321e07a44527a229de01fff7fbe
MarkS <marksvc@gmail.com>
2020-03-19 14:33:43 -0500

    Add download.mono-project.com to pbuilder chroot
    I did the work for this and then didn't need it. But probably we will
    sometime soon.
    
    Change-Id: Idf31ff45ea307727b5eafb226b0d4a84e06669db

--- packaging/pbuilder/setup.sh
+++ packaging/pbuilder/setup.sh
@@ -44,6 +44,7 @@ KEYRINGLLSO="$PBUILDERDIR/llso-keyring-2013.gpg"
 KEYRINGPSO="$PBUILDERDIR/pso-keyring-2016.gpg"
 KEYRINGNODE="$PBUILDERDIR/nodesource-keyring.gpg"
 KEYRINGMICROSOFT="$PBUILDERDIR/microsoft.asc.gpg"
+KEYRING_MONO="$PBUILDERDIR/mono-project.asc.gpg"
 
 if [ ! -f ${KEYRINGPSO} ]; then
 	wget --output-document=${KEYRINGPSO} https://packages.sil.org/keys/pso-keyring-2016.gpg
@@ -69,6 +70,13 @@ if [ ! -f ${KEYRINGMICROSOFT} ]; then
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o ${KEYRINGMICROSOFT}
 fi
 
+if [ ! -f ${KEYRING_MONO} ]; then
+	TMP_KEYRING=$(mktemp)
+	XAMARIN_KEY_FINGERPRINT="3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+	gpg --no-default-keyring --keyring ${TMP_KEYRING} --recv-keys ${XAMARIN_KEY_FINGERPRINT}
+	gpg --no-default-keyring --keyring ${TMP_KEYRING} --export > ${KEYRING_MONO}
+fi
+
 for D in ${DISTRIBUTIONS:-$UBUNTU_DISTROS $UBUNTU_OLDDISTROS $DEBIAN_DISTROS}
 do
 	for A in ${ARCHES-amd64 i386}
@@ -84,8 +92,9 @@ do
 
 		CheckOrLinkDebootstrapScript $D
 
-		# 64-bit only repo. 32-bit can be downloaded as a tar.
+		# packages.microsoft is a 64-bit only repo. 32-bit can be downloaded as a tar.
 		MICROSOFT_APT="deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-${D}-prod ${D} main"
+		MONO_APT="deb https://download.mono-project.com/repo/ubuntu vs-${D} main"
 
 		if [[ "$UBUNTU_DISTROS $UBUNTU_OLDDISTROS" == *$D* ]]; then
 			if [[ $UBUNTU_DISTROS == *$D* ]]; then
@@ -113,6 +122,7 @@ do
 					# Adding apt-transport-https to pbuilder --debootstrapopts --include does not solve it.
 					addmirror "deb https://deb.nodesource.com/node_8.x $D main"
 					addmirror "${MICROSOFT_APT}"
+					addmirror "${MONO_APT}"
 				fi
 			fi
 		elif [[ $DEBIAN_DISTROS == *$D* ]]; then
@@ -131,6 +141,7 @@ do
 					# isn't available yet. This is so for Debian stretch, but beginning in Debian buster the capability is probably built-in.
 					addmirror "deb https://deb.nodesource.com/node_8.x $D main"
 					addmirror "${MICROSOFT_APT}"
+					addmirror "${MONO_APT}"
 				fi
 			fi
 		else
@@ -151,6 +162,7 @@ do
 			${KEYRINGPSO:+--keyring }$KEYRINGPSO \
 			${KEYRINGNODE:+--keyring }$KEYRINGNODE \
 			${KEYRINGMICROSOFT:+--keyring }$KEYRINGMICROSOFT \
+			${KEYRING_MONO:+--keyring }$KEYRING_MONO \
 			--extrapackages "apt-utils devscripts lsb-release apt-transport-https ca-certificates tzdata" \
 			--othermirror "$OTHERMIRROR" \
 			--mirror "$MIRROR" \
