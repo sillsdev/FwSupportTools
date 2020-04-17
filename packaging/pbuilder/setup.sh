47a42f36b840a686fa7c2d8e631db6c35c0ed8b6
MarkS <marksvc@gmail.com>
2020-04-17 15:30:40 -0500

    [Packaging] Apply --trust-model always elsewhere
    Change-Id: I6707d2ee3fdc9790dd09c63e32a3c7e2202b0db9

--- packaging/pbuilder/setup.sh
+++ packaging/pbuilder/setup.sh
@@ -58,11 +58,14 @@ if [ ! -f ${KEYRINGNODE} ]; then
 	# Download node key and convert to keyring.
 
 	NODE_KEY="$(mktemp -d)/nodesource-key.asc"
-	# Use a temporary, intermediate keyring since it may be keybox format on Ubuntu 20.04, and we need it to be an older format for apt.
+	# Use a temporary, intermediate keyring since it may be keybox format on Ubuntu 20.04,
+	# and we need it to be an older format for apt.
 	TMP_KEYRING=$(mktemp)
 	wget --output-document=${NODE_KEY} https://deb.nodesource.com/gpgkey/nodesource.gpg.key
-	gpg --no-default-keyring --keyring ${TMP_KEYRING} --import ${NODE_KEY}
-	# Export without --armor since gpg seems to have trouble inspecting an armor export keyring.
+	gpg --trust-model always --no-default-keyring --keyring ${TMP_KEYRING} \
+		--import ${NODE_KEY}
+	# Export without --armor since gpg seems to have trouble inspecting an armor export
+	# keyring.
 	gpg --no-default-keyring --keyring ${TMP_KEYRING} --export > ${KEYRINGNODE}
 fi
 
