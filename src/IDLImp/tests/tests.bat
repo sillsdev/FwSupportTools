3a6f7f5df759835df13cf9027c19646aceaf6ac6
Eberhard Beilharz <eb1@sil.org>
2022-06-23 11:21:29 +0200

    Remove IDLImp code
    IDLImp is now a separate project on GitHub:
    https://github.com/sillsdev/IdlImporter.
    
    Change-Id: I4bb553fcba50c598d99ed18af5d212c48b28a00d

--- src/IDLImp/tests/tests.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call DoTest.bat FwKernelTlb.idl FwKernelTlb.cs
-call DoTest.bat ViewsTlb.idl ViewsTlb.cs
-call DoTest.bat DbAccessTlb.idl DbAccessTlb.cs
-call DoTest.bat MigrateDataTlb.idl MigrateDataTlb.cs
-call DoTest.bat FwCellarTlb.idl FwCellarTlb.cs
-call DoTest.bat LanguageTlb.idl LanguageTlb.cs
