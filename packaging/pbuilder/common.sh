c1aff8be725618b1ee6c6e95574a102183c3e544
MarkS <marksvc@gmail.com>
2024-01-29 10:10:50 -0700

    [Packaging] Add release codename noble
    Change-Id: Ifb8cd8368e6a74c8c918765e2a80d8bf935a4dbb

--- packaging/pbuilder/common.sh
+++ packaging/pbuilder/common.sh
@@ -6,7 +6,7 @@ init()
 	[ -e $CONFIGFILE ] && . $CONFIGFILE # Settings for PBUILDERDIR, DISTRIBUTIONS, etc.
 
 	# currently supported and future Ubuntu versions
-	UBUNTU_DISTROS="devel trusty xenial bionic cosmic disco eoan focal groovy hirsute impish jammy kinetic lunar mantic"
+	UBUNTU_DISTROS="devel trusty xenial bionic cosmic disco eoan focal groovy hirsute impish jammy kinetic lunar mantic noble"
 	# no longer supported Ubuntu versions that live in old-releases.ubuntu.com
 	UBUNTU_OLDDISTROS=""
 	# We're no longer building packages for: precise quantal raring saucy utopic vivid wily yakkety zesty artful
